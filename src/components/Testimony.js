dd0662a40366798e027adbf68ffafe24eee5ee9b
Aileen Alvarado <monsealvarado775@gmail.com>
2024-01-03 16:27:45 -0600

    App creation

---
+++ src/components/Testimony.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Testimony() {
+  return (
+    <div className='testimony-container'>
+      <img 
+        className='testimony-image'
+        src={('../images/senora1.jpg')}
+        alt='Imagen de una senora' 
+      />
+    </div>
+  );
+}
