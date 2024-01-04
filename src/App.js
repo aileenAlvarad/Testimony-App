06194819f78d736b5ddc90ded1b0eb7e7d9205f9
Aileen Alvarado <monsealvarado775@gmail.com>
2024-01-04 17:01:35 -0600

    Creating the first component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimony from './components/Testimony'
 
 function App() {
   return (
-    <div className="App"> 
-
+    <div className="App">  
+    <Testimony />
     </div>
+    
   );
 }
 
--- src/components/Testimony.js
+++ src/components/Testimony.js
@@ -1,13 +1,24 @@
 import React from "react";
+import image from '../images/senora1.jpg';
 
 function Testimony() {
   return (
     <div className='testimony-container'>
-      <img 
+      <img
         className='testimony-image'
-        src={('../images/senora1.jpg')}
-        alt='Imagen de una senora' 
+        src={image}
+        alt='Imagen de una senora'
       />
+      <div className='text-container-testimony'>
+        <p className='testimony-name'>Dora Maria en   México</p>
+        <p className='testimony-position'>Dulcería la Bodeguita</p>
+        <p className='testimony-text'>Si buscas piñatas de alta
+          calidad que agreguen diversión y alegría a cualquier celebración,
+          ¡este es definitivamente el lugar para encontrarlas! Recomiendo encarecidamente
+          estas piñatas a todos los que quieran asegurarse de tener una fiesta inolvidable.</p>
+      </div>
     </div>
   );
 }
+
+export default Testimony;
