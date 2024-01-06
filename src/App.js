06f0b8739116a56259a66cd432a82fa2e7454901
Aileen Alvarado <monsealvarado775@gmail.com>
2024-01-05 18:04:01 -0600

    Adding styles to components

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import Testimony from './components/Testimony'
 
 function App() {
   return (
-    <div className="App">  
-    <Testimony />
+    <div className="App"> 
+      <div className='main-container'>
+      <h1>Esto es lo que dicen nuestros clientes sobre las piñatas:</h1>  
+      <Testimony />
+      </div>
     </div>
     
   );
--- src/components/Testimony.js
+++ src/components/Testimony.js
@@ -1,5 +1,6 @@
 import React from "react";
-import image from '../images/senora1.jpg';
+import image from '../images/senora1.jpg'; 
+import '../stylesheets/Testimony.css';
 
 function Testimony() {
   return (
@@ -12,10 +13,10 @@ function Testimony() {
       <div className='text-container-testimony'>
         <p className='testimony-name'>Dora Maria en   México</p>
         <p className='testimony-position'>Dulcería la Bodeguita</p>
-        <p className='testimony-text'>Si buscas piñatas de alta
+        <p className='testimony-text'>"Si buscas piñatas de alta
           calidad que agreguen diversión y alegría a cualquier celebración,
           ¡este es definitivamente el lugar para encontrarlas! Recomiendo encarecidamente
-          estas piñatas a todos los que quieran asegurarse de tener una fiesta inolvidable.</p>
+          estas piñatas a todos los que quieran asegurarse de tener una fiesta inolvidable."</p>
       </div>
     </div>
   );
