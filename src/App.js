4da5da37a3fa4de8b635076afb4ab3766f5f66a7
Aileen Alvarado <monsealvarado775@gmail.com>
2024-01-08 17:19:42 -0600

    Adding more components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,34 @@ function App() {
     <div className="App"> 
       <div className='main-container'>
       <h1>Esto es lo que dicen nuestros clientes sobre las piñatas:</h1>  
-      <Testimony />
+      <Testimony 
+        name='Dora Dominga'
+        country='Mexico' 
+        image='uno' 
+        position='Propietaria ' 
+        company='Dulceria Bodeguita' 
+        testimony='Si buscas piñatas de alta calidad que agreguen diversión y alegría a cualquier celebración, ¡este es definitivamente el lugar para encontrarlas! Recomiendo encarecidamente estas piñatas a todos los que quieran asegurarse de tener una fiesta inolvidable.'/> 
+     
+      <Testimony
+      name='Patricia Vazquez'
+        country='Mexico' 
+        image='tres' 
+        position='Licenciada en Economia ' 
+        company='Bancomer' 
+        testimony='Lo que más me impresionó fue la atención a los detalles. Cada piñata estaba cuidadosamente
+         decorada con colores vibrantes y diseños encantadores. 
+         ¡Eran casi demasiado bonitas para romperlas! Pero una vez que los niños
+          empezaron a jugar con ellas, quedó claro que estaban hechas para la diversión.'/>
+      <Testimony
+      name='Patricia Vazquez'
+        country='Mexico' 
+        image='cinco' 
+        position='Licenciada en Economia ' 
+        company='Bancomer' 
+        testimony='Lo que más me impresionó fue la atención a los detalles. Cada piñata estaba cuidadosamente
+         decorada con colores vibrantes y diseños encantadores. 
+         ¡Eran casi demasiado bonitas para romperlas! Pero una vez que los niños
+          empezaron a jugar con ellas, quedó claro que estaban hechas para la diversión.'/>
       </div>
     </div>
     
--- src/components/Testimony.js
+++ src/components/Testimony.js
@@ -1,22 +1,19 @@
 import React from "react";
-import image from '../images/senora1.jpg'; 
+//import image from '../images/testimony-cuatro.jpg'; 
 import '../stylesheets/Testimony.css';
 
-function Testimony() {
+function Testimony(props) {
   return (
     <div className='testimony-container'>
       <img
         className='testimony-image'
-        src={image}
+        src={require(`../images/testimony-${props.image}.jpg`)}
         alt='Imagen de una senora'
       />
       <div className='text-container-testimony'>
-        <p className='testimony-name'>Dora Maria en   México</p>
-        <p className='testimony-position'>Dulcería la Bodeguita</p>
-        <p className='testimony-text'>"Si buscas piñatas de alta
-          calidad que agreguen diversión y alegría a cualquier celebración,
-          ¡este es definitivamente el lugar para encontrarlas! Recomiendo encarecidamente
-          estas piñatas a todos los que quieran asegurarse de tener una fiesta inolvidable."</p>
+        <p className='testimony-name'>{props.name} en {props.pais} </p>
+        <p className='testimony-position'>{props.position} {props.company} </p>
+        <p className='testimony-text'>"{props.testimony}"</p>
       </div>
     </div>
   );
